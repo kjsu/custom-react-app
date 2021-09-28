8743ebb61df0c28f944efce4b2309f9dafd52fff
kjsu <surtne@gmail.com>
2021-09-29 03:29:45 +0900

    precommit test

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -5,6 +5,7 @@ const Home: React.FC = () => {
   const test4 = "check"; const test5 = "check1";
   const test6 = "check";;;;;;;;;;;;;;;;
           const test7 = "check";;;;;;;;;;;;;;;;
+          const test8 = "check";;;;;;;;;;;;;;;;
 
   return (
     <>home</>
