f09671b1d6dbefcca6bfbae871a1d398a85c4459
kjsu <surtne@gmail.com>
2021-09-29 04:46:18 +0900

    precommit test

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -1,31 +1,7 @@
 import React from 'react'
 
 const Home: React.FC = () => {
-        const test = "check"; const test2 = "check1";;;;;;;;;;;;;;;;;;;;
-  const test4 = "check"; const test5 = "check1";
-  const test6 = "check";;;;;;;;;;;;;;;;
-          const test7 = "check";;;;;;;;;;;;;;;;
-          const test8 = "check";;;;;;;;;;;;;;;;
-          const test9 = "check";;;;;;;;;;;;;;;;
-
-          const test10 = "check";;;;;;;;;;;;;;;;
-
-          const test11 = "check";;;;;;;;;;;;;;;;
-
-          const test12 = "check";;;;;;;;;;;;;;;;
-
-          const test13 = "check";;;;;;;;;;;;;;;;
-
-          const test14 = "check";;;;;;;;;;;;;;;;
-
-          const test15 = "check";;;;;;;;;;;;;;;;
-          const test16 = "check";;;;;;;;;;;;;;;;
-
-
-
-
-
-
+  if (0) { console.log("test");;;;;;;;}
 
   return (
     <>home</>
