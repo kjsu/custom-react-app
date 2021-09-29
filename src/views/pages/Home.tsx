479f5dfe79be6c8b65b6bc3d5d89fd89935e22c4
kjsu <surtne@gmail.com>
2021-09-29 21:39:54 +0900

    sample useRecoilState

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect } from 'react'
 import sampleService from '~/services/sampleService' /* recoil sample용 임시 코드, 실제 구현은 action 사용 */
+import { useRecoilState } from 'recoil'
+import { sampleAtom } from '~/recoil/sampleAtom'
 
 const Home: React.FC = () => {
+  const [sample, setSample] = useRecoilState(sampleAtom)
+
   useEffect(() => {
-    sampleService.sampleRecoilService()
+    // sampleService.sampleRecoilService()
     // sampleService.sampleGetRecoilService()
+    setSample({ info: 'test' })
   }, [])
 
   return (
-    <>home</>
+    <>home {sample.info}</>
   )
 }
 
